0e983baab30b90e6fbbf73405d04e07ca070dfb7
Edward Tate <cmalune@gmail.com>
2018-05-02 05:35:05 +0100

    Added lagrange.

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@ pub use group::Group;
 pub use integer::Integer;
 pub use pairing::Pairing;
 
+use std::collections::HashSet;
+
 pub fn xor(a: Vec<u8>, b: Vec<u8>) -> Vec<u8> {
     assert_eq!(a.len(), b.len());
 
@@ -48,4 +50,37 @@ pub fn polynomial(in_x: &Element, pairing: &Pairing, coefficients: &Vec<Element>
 
     // ret y
     y
+}
+
+pub fn lagrange(pairing: &Pairing, k: u32, l: u32, s: &HashSet<i64>, j: i64) -> Element {
+    assert_eq!(k as usize, s.len());
+
+    let expected_s: HashSet<i64> = (0..l).map(|i| i as i64).collect();
+
+    assert!(s.is_subset(&expected_s));
+
+    let mut s: Vec<i64> = s.iter().cloned().filter(|x| (*x) != j.clone()).collect();
+
+    s.sort();
+
+    // num = 1
+    let mut num = Element::new(Group::Zr, pairing);
+    num.set1();
+    // den = 1
+    let mut den = Element::new(Group::Zr, pairing);
+    den.set1();
+
+    for jj in s.iter() {
+        // num *= 0 - jj.clone() - 1;;
+        let mut num_t = Element::new(Group::Zr, pairing);
+        num_t.set_si(0 - jj.clone() - 1);
+        num.mul(&num_t);
+        let mut den_t = Element::new(Group::Zr, pairing);
+        // den *= j.clone() - jj.clone();;
+        den_t.set_si(j.clone() - jj.clone());
+        den.mul(&den_t);
+    }
+
+    num.div(&den);
+    num
 }
